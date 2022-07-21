0927c842f5104e28df598ad3e88020f590d0a418
lele9h0st <hoangtranhuy2001@gmail.com>
2022-07-21 20:34:32 +0700

    test sign in with jwt

--- src/app/signin/signin-form/signin-form.component.ts
+++ src/app/signin/signin-form/signin-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthService} from "../../_service/auth.service";
+import {TokenStorageService} from "../../_service/token-storage.service";
 
 @Component({
   selector: 'app-signin-form',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin-form.component.scss']
 })
 export class SigninFormComponent implements OnInit {
+  public siginForm !: FormGroup;
+  isLoggedIn = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private authService: AuthService, private tokenStorage: TokenStorageService) {
   }
 
+  ngOnInit(): void {
+    this.siginForm = this.formBuilder.group({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+      rememberCheck: new FormControl("")
+    })
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  sigin() {
+    // this.http.get<any>("http://localhost:3000/user").subscribe(res => {
+    //   const user = res.find((a: any) => {
+    //     return a.username === this.siginForm.value.username && a.password === this.siginForm.value.password
+    //   })
+    //   if (user) {
+    //     alert("login success")
+    //     this.siginForm.reset();
+    //   }
+    // })
+
+    this.authService.login(this.siginForm.value['username'],this.siginForm.value.password).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(this.siginForm.value['username'],this.siginForm.value.password);
+        this.isLoggedIn = true;
+        alert("login success")
+        window.location.reload();
+      },
+      err => {
+        // this.errorMessage = err.error.message;
+        // this.isLoginFailed = true;
+        alert("login fail")
+      }
+    );
+  }
 }
