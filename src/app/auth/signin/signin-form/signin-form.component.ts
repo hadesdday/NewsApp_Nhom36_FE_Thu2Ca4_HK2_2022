5369bbcfd582397c3f242257e4e8238ec51a380a
lele9h0st <hoangtranhuy2001@gmail.com>
2022-08-23 02:14:15 +0700

    #4 - remember account

--- src/app/auth/signin/signin-form/signin-form.component.ts
+++ src/app/auth/signin/signin-form/signin-form.component.ts
@@ -13,16 +13,18 @@ import {API_AUTH} from 'src/app/_api/apiURL';
 })
 export class SigninFormComponent implements OnInit {
   public siginForm !: FormGroup;
-
+  rememberAccount: any;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router, private authService: AuthService, private tokenStorage: TokenStorageService) {
   }
 
   ngOnInit(): void {
+    this.rememberAccount = this.tokenStorage.getRememberAccount()
+
     this.siginForm = this.formBuilder.group({
-      email: new FormControl("", [Validators.required, Validators.email]),
-      password: new FormControl("", [Validators.required]),
-      rememberCheck: new FormControl("")
+      email: new FormControl(this.rememberAccount.email, [Validators.required, Validators.email]),
+      password: new FormControl(this.rememberAccount.password, [Validators.required]),
+      rememberCheck: new FormControl(this.rememberAccount!={})
     })
   }
 
