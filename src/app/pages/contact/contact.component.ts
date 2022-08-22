de8b0f8cf1a436c404213b1da15e765fc4205b4a
lele9h0st <hoangtranhuy2001@gmail.com>
2022-08-23 03:04:42 +0700

    #4 - add contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {API_AUTH, API_URL} from "../../_api/apiURL";
+import {CommonService} from "../../_service/common.service";
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient,private commonService: CommonService, private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      name: new FormControl("", [Validators.required]),
+      title: new FormControl("", [Validators.required]),
+      text: new FormControl("", [Validators.required])
+    })
+  }
+
+  contact() {
+    this.http.post<any>(API_URL.CONTACT, {
+      "email": this.contactForm.value.email,
+      "name": this.contactForm.value.name,
+      "title": this.contactForm.value.title,
+      "text": this.contactForm.value.text
+    }).subscribe(res => {
+      this.commonService.toastSuccess("Gửi tin thành công thành công!")
+      window.location.reload();
+    },error => {
+      this.commonService.toastError("Gửi tin thất bại!!")
+    });
+  }
 }
